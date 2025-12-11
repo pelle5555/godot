901f5f787a62a2275230b9154bbcee471a6f8049
Niklas Pelle <nicolaum1609@gmail.com>
2025-12-11 19:40:07 +0100

    did lecture 3.8 and 3.9

--- parbarian-plaster/Scripts/base.gd
+++ parbarian-plaster/Scripts/base.gd
@@ -1,6 +1,18 @@
 extends Node3D
 
+@export var max_health: int = 5
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		print("health was changed")
+		label_3d.text = str(current_health)
+		if current_health < 1:
+			get_tree().reload_current_scene()
 
+@onready var label_3d: Label3D = $Label3D
 
-func take_damage() -> void:
-	print("Damage delt to base!")
+func _ready() -> void:
+	current_health = max_health
+	
+func take_damage(damage) -> void:
+	current_health -= damage
--- parbarian-plaster/Scripts/enemy.gd
+++ parbarian-plaster/Scripts/enemy.gd
@@ -1,13 +1,14 @@
 extends PathFollow3D
 
-@export var enemySpeed: float = 5
+@export var enemy_speed: float = 10
+@export var enemy_damage: int = 1
 
 @onready var base = get_tree().get_first_node_in_group("base")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	progress += delta * enemySpeed
+	progress += delta * enemy_speed
 	
 	if progress_ratio == 1.0:
-		base.take_damage()
+		base.take_damage(enemy_damage)
 		self.queue_free()
