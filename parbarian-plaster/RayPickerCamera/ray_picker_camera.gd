85bb2404bd98e834ea8627f12aea5ca09d9db3a6
Niklas Pelle <nicolaum1609@gmail.com>
2025-12-13 14:04:20 +0100

    3.11 3.12 3.13

---
+++ parbarian-plaster/RayPickerCamera/ray_picker_camera.gd
@@ -0,0 +1,27 @@
+extends Camera3D
+
+@export var raycast_distance: float = 100.0
+@export var gridmap: GridMap
+
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var mouse_posision: Vector2 = get_viewport().get_mouse_position()
+	ray_cast_3d.target_position = project_local_ray_normal(mouse_posision) * raycast_distance
+	ray_cast_3d.force_raycast_update()
+	
+	
+	if(ray_cast_3d.is_colliding()):
+		Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+		var collider = ray_cast_3d.get_collider() 
+		if collider is GridMap:
+			if(Input.is_action_just_pressed("mouse_left_click")):
+				var collision_point = ray_cast_3d.get_collision_point()
+				var cell = gridmap.local_to_map(collision_point)
+				if(gridmap.get_cell_item(cell) == 0):
+					gridmap.set_cell_item(cell, 1)
+	else:
+		Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+		
+		
+	
