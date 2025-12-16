a70d7040c22455ab337a6fe1a0f7dfc19f690dea
Niklas Pelle <nicolaum1609@gmail.com>
2025-12-16 19:20:35 +0100

    3,17

--- parbarian-plaster/Scripts/turret.gd
+++ parbarian-plaster/Scripts/turret.gd
@@ -1,8 +1,16 @@
 extends Node3D
 
-
 @export var projectile: PackedScene
 
-func _ready() -> void:
+var enemy_path: Path3D
+
+@onready var turret_barrel: MeshInstance3D = $TurretBase/TurretHead/TurretBarrel
+
+func _physics_process(delta: float) -> void:
+	var enemy = enemy_path.get_children().back()
+	look_at(enemy.global_position, Vector3.UP, true)
+
+func _on_timer_timeout() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)
+	shot.global_position = turret_barrel.global_position
